906f2471fd8b269589e47d638e23041d1c408b9b
Lena Ebner <lena.ebner@hublz.art>
2025-05-05 16:05:44 +0200

    feat: update location tca have consistent field names

--- packages/surfcamp-events/Configuration/TCA/tx_surfcamp_events_location.php
+++ packages/surfcamp-events/Configuration/TCA/tx_surfcamp_events_location.php
@@ -20,10 +20,10 @@ return [
         ],
     ],
     'interface' => [
-        'showRecordFieldList' => 'name, street, street_nr, zip, city, country, long, lat',
+        'showRecordFieldList' => 'name, street, street_nr, postal_code, city, country, longitude, latitude',
     ],
     'types' => [
-        '1' => ['showitem' => 'name, street, street_nr, zip, city, country, long, lat'],
+        '1' => ['showitem' => 'name, street, street_nr, postal_code, city, country, longitude, latitude'],
     ],
     'columns' => [
         'name' => [
@@ -41,13 +41,13 @@ return [
             ],
         ],
         'street_nr' => [
-            'label' => 'Street no',
+            'label' => 'Street Nr',
             'config' => [
                 'type' => 'input',
                 'eval' => 'required,trim',
             ],
         ],
-        'zip' => [
+        'postal_code' => [
             'label' => 'Post code',
             'config' => [
                 'type' => 'input',
@@ -68,18 +68,18 @@ return [
                 'eval' => 'required,trim',
             ],
         ],
-        'lat' => [
+        'latitude' => [
             'label' => 'Latitute',
             'config' => [
-                'type' => 'number',
-                'format' => 'decimal',
+                'type' => 'input',
+                'eval' => 'double2',
             ],
         ],
-        'long' => [
+        'longitude' => [
             'label' => 'Longitude',
             'config' => [
-                'type' => 'number',
-                'format' => 'decimal',
+                'type' => 'input',
+                'eval' => 'double2',
             ],
         ],
         'timezone' => [
