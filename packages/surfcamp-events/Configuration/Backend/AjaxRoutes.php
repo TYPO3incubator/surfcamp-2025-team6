b64d66d86107fd69ca597ddbe804a2d6b47854cf
Niels Langlotz <me@typoniels.de>
2025-05-08 18:03:36 +0100

    feat: Add Appointment-Generation JSON-Editor, Small Improvements

--- packages/surfcamp-events/Configuration/Backend/AjaxRoutes.php
+++ packages/surfcamp-events/Configuration/Backend/AjaxRoutes.php
@@ -3,8 +3,8 @@
 use TYPO3Incubator\SurfcampEvents\Controller\Backend\EventGeneratorBackendModuleController;
 
 return [
-    'surfcamp_events_appointments_pregenerate' => [
-        'path' => '/events-management/appointments/pregenerate',
-        'target' => EventGeneratorBackendModuleController::class . '::pregenerateAppointments',
+    'surfcamp_events_appointments_generate' => [
+        'path' => '/events-management/appointments/generate',
+        'target' => EventGeneratorBackendModuleController::class . '::generateAppointments',
     ],
 ];
