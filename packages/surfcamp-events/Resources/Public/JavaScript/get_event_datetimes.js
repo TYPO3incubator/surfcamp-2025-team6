cc93c049508c286c693a78b83e4341485b06e893
Rafal Jania <rj@studiomitte.com>
2025-05-09 00:53:59 +0200

    feat: calculate time for appointments

--- packages/surfcamp-events/Resources/Public/JavaScript/get_event_datetimes.js
+++ packages/surfcamp-events/Resources/Public/JavaScript/get_event_datetimes.js
@@ -1,7 +1,11 @@
 window.onload = function() {
-    const ids = getEventIds();
     const timezone = getClientTimezone();
-    fetchEventDates(ids, timezone);
+
+    const eventIds = getEventIds();
+    fetchEventDates(eventIds, timezone);
+
+    const appointmentIds = getAppointmentIds();
+    fetchAppointmentDates(appointmentIds, timezone);
 };
 
 function getEventIds() {
@@ -13,6 +17,17 @@ function getEventIds() {
         }
     })
     return eventIds;
+}
+
+function getAppointmentIds() {
+    const appointments = document.querySelectorAll('.appointment');
+    let appointmentIds = [];
+    appointments.forEach(function (appointment) {
+        if (appointment.dataset.id) {
+            appointmentIds.push(appointment.dataset.id);
+        }
+    })
+    return appointmentIds;
 }
 
 function getClientTimezone() {
@@ -56,6 +71,48 @@ function updateEventsDateTime(data) {
         if(eventData.endDate) {
             let dateTime = new Date(eventData.endDate);
             const endDateElement = eventElement.querySelector('.event-end-date');
+            endDateElement.innerHTML = formatDate(dateTime);
+        }
+    });
+}
+
+async function fetchAppointmentDates(ids, timezone) {
+    const url = '/api/surfcamp-events/get-time-for-appointments?ids=' + ids.join() + '&timezone=' + timezone;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        updateAppointmentsDateTime(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+function updateAppointmentsDateTime(data) {
+    const appointments = document.querySelectorAll('.appointment');
+    appointments.forEach(function (appointmentElement) {
+        if (!appointmentElement.dataset.id) {
+            return;
+        }
+
+        if (!data[appointmentElement.dataset.id]) {
+            return;
+        }
+
+        const appointmentData = data[appointmentElement.dataset.id];
+
+        if(appointmentData.startDate) {
+            let dateTime = new Date(appointmentData.startDate);
+            const startDateElement = appointmentElement.querySelector('.event-start-date');
+            startDateElement.innerHTML = formatDate(dateTime);
+        }
+
+        if(appointmentData.endDate) {
+            let dateTime = new Date(appointmentData.endDate);
+            const endDateElement = appointmentElement.querySelector('.event-end-date');
             endDateElement.innerHTML = formatDate(dateTime);
         }
     });
