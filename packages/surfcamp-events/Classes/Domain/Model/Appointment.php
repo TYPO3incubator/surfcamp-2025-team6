e2cbdc871e39c8dd2828826878242debb7031f59
Rafal Jania <rj@studiomitte.com>
2025-05-08 19:26:24 +0200

    feat: calculate datetime for appoimntments

--- packages/surfcamp-events/Classes/Domain/Model/Appointment.php
+++ packages/surfcamp-events/Classes/Domain/Model/Appointment.php
@@ -11,6 +11,36 @@ class Appointment extends AbstractDomainObject
      */
     protected string $title = '';
 
+    /**
+     * @var \DateTime|null
+     */
+    protected ?\DateTime $start_date_time = null;
+
+    /**
+     * @var \DateTime|null
+     */
+    protected ?\DateTime $start_date_time_utc = null;
+
+    /**
+     * @var \DateTime|null
+     */
+    protected ?\DateTime $end_date_time = null;
+
+    /**
+     * @var \DateTime|null
+     */
+    protected ?\DateTime $end_date_time_utc = null;
+
+    /**
+     * @var string
+     */
+    protected string $timezone = '';
+
+    /**
+     * @var string
+     */
+    protected string $tzdb_version = '';
+
     public function getTitle(): string
     {
         return $this->title;
@@ -19,5 +49,65 @@ class Appointment extends AbstractDomainObject
     public function setTitle(string $title): void
     {
         $this->title = $title;
+    }
+
+    public function getStartDateTime(): ?\DateTime
+    {
+        return $this->start_date_time;
+    }
+
+    public function setStartDateTime(?\DateTime $start_date_time): void
+    {
+        $this->start_date_time = $start_date_time;
+    }
+
+    public function getStartDateTimeUtc(): ?\DateTime
+    {
+        return $this->start_date_time_utc;
+    }
+
+    public function setStartDateTimeUtc(?\DateTime $start_date_time_utc): void
+    {
+        $this->start_date_time_utc = $start_date_time_utc;
+    }
+
+    public function getEndDateTime(): ?\DateTime
+    {
+        return $this->end_date_time;
+    }
+
+    public function setEndDateTime(?\DateTime $end_date_time): void
+    {
+        $this->end_date_time = $end_date_time;
+    }
+
+    public function getEndDateTimeUtc(): ?\DateTime
+    {
+        return $this->end_date_time_utc;
+    }
+
+    public function setEndDateTimeUtc(?\DateTime $end_date_time_utc): void
+    {
+        $this->end_date_time_utc = $end_date_time_utc;
+    }
+
+    public function getTimezone(): string
+    {
+        return $this->timezone;
+    }
+
+    public function setTimezone(string $timezone): void
+    {
+        $this->timezone = $timezone;
+    }
+
+    public function getTzdbVersion(): string
+    {
+        return $this->tzdb_version;
+    }
+
+    public function setTzdbVersion(string $tzdb_version): void
+    {
+        $this->tzdb_version = $tzdb_version;
     }
 }
--- packages/surfcamp-events/Classes/Middleware/TimezoneApiMiddleware.php
+++ packages/surfcamp-events/Classes/Middleware/TimezoneApiMiddleware.php
@@ -10,7 +10,9 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Http\Response;
 use TYPO3\CMS\Core\Http\Stream;
+use TYPO3Incubator\SurfcampEvents\Domain\Model\Appointment;
 use TYPO3Incubator\SurfcampEvents\Domain\Model\Event;
+use TYPO3Incubator\SurfcampEvents\Domain\Repository\AppointmentRepository;
 use TYPO3Incubator\SurfcampEvents\Domain\Repository\EventRepository;
 use TYPO3Incubator\SurfcampEvents\Service\EventDatetimeService;
 use TYPO3Incubator\SurfcampEvents\Service\TimezoneService;
@@ -20,6 +22,7 @@ class TimezoneApiMiddleware implements MiddlewareInterface
     public function __construct(
         private TimezoneService $timezoneService,
         private EventRepository $eventRepository,
+        private AppointmentRepository $appointmentRepository,
         private EventDatetimeService $eventDatetimeService
     ) {}
 
@@ -29,14 +32,15 @@ class TimezoneApiMiddleware implements MiddlewareInterface
         $uri = $request->getUri();
         $path = $uri->getPath();
 
-        if ($path !== '/api/surfcamp-events/get-time-for-events') {
+        if ($path !== '/api/surfcamp-events/get-time-for-events'
+            && $path !== '/api/surfcamp-events/get-time-for-appointments') {
             return $handler->handle($request);
         }
 
         if (!array_key_exists('ids', $request->getQueryParams())) {
             return (new Response())->withStatus(400);
         }
-        $eventIds = explode(',', $request->getQueryParams()['ids']);
+        $ids = explode(',', $request->getQueryParams()['ids']);
 
         if (array_key_exists('timezone', $request->getQueryParams())) {
             $timezone = $request->getQueryParams()['timezone'];
@@ -44,6 +48,28 @@ class TimezoneApiMiddleware implements MiddlewareInterface
             $timezone = $this->timezoneService->getUserTimezone();
         }
 
+        switch ($path) {
+            case '/api/surfcamp-events/get-time-for-events':
+                $data = $this->processEvents($ids, $timezone);
+                break;
+            case '/api/surfcamp-events/get-time-for-appointments':
+                $data = $this->processAppointments($ids, $timezone);
+                break;
+            default:
+                $data = [];
+        }
+
+        $body = new Stream('php://temp', 'rw');
+        $body->write(json_encode($data));
+
+        return (new Response())
+            ->withHeader('content-type', 'application/json; charset=utf-8')
+            ->withBody($body)
+            ->withStatus(200);
+    }
+
+    public function processEvents(array $eventIds, string $timezone): array
+    {
         $query = $this->eventRepository->createQuery();
         $events = $query
             ->matching($query->in('uid', $eventIds))
@@ -60,13 +86,27 @@ class TimezoneApiMiddleware implements MiddlewareInterface
             ];
         }
 
-        $body = new Stream('php://temp', 'rw');
-        $body->write(json_encode($data));
-
-        return (new Response())
-            ->withHeader('content-type', 'application/json; charset=utf-8')
-            ->withBody($body)
-            ->withStatus(200);
+        return $data;
     }
 
+    public function processAppointments(array $eventIds, string $timezone): array
+    {
+        $query = $this->appointmentRepository->createQuery();
+        $appointments = $query
+            ->matching($query->in('uid', $eventIds))
+            ->execute();
+
+        $data = [];
+        /** @var Appointment $appointment */
+        foreach ($appointments as $appointment) {
+            $data[$appointment->getUid()] = [
+                'eventId' => $appointment->getUid(),
+                'startDate' => $this->eventDatetimeService->getAppointmentStartDateInTimezone($appointment, $timezone),
+                'endDate' => $this->eventDatetimeService->getAppointmentEndDateInTimezone($appointment, $timezone),
+                'timezone' => $timezone
+            ];
+        }
+
+        return $data;
+    }
 }
